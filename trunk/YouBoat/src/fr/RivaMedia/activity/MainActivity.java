f3889eaf21b550496a87485f7071223ce8aae5db
kevindejesusferreira@gmail.com <kevindejesusferreira@gmail.com@4e780220-1cfd-1e42-7943-0f62b68f17dc>
2013-11-11 19:12:00 +0000

    Button in sold change with onclick

--- trunk/YouBoat/src/fr/RivaMedia/activity/MainActivity.java
+++ trunk/YouBoat/src/fr/RivaMedia/activity/MainActivity.java
@@ -157,23 +157,28 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 		transaction.addToBackStack(null);
 		transaction.commit();
 	}
-	public void afficherAnnuaire(){}
+	public void afficherAnnuaire(){
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		transaction.replace(R.id.main_fragment, new Annuaire());
+		transaction.addToBackStack(null);
+		transaction.commit();	
+	}
 	public void afficherMesAnnonces(){}
-	
+
 	public void afficherMesAlertes(){
 		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 		transaction.add(R.id.main_fragment, new MesAlertes());
 		transaction.addToBackStack(null);
 		transaction.commit();
 	}
-	
+
 	public void afficherInformations(){
 		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 		transaction.add(R.id.main_fragment, new Informations());
 		transaction.addToBackStack(null);
 		transaction.commit();
 	}
-	
+
 	public void afficherContactPro(){
 		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 		transaction.add(R.id.main_fragment, new ContactPro());
--- trunk/YouBoat/src/fr/RivaMedia/fragments/BoatOnDemand.java
+++ trunk/YouBoat/src/fr/RivaMedia/fragments/BoatOnDemand.java
@@ -6,15 +6,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import fr.RivaMedia.R;
 
 @SuppressLint("ValidFragment")
 public class BoatOnDemand extends Fragment{
 
+	private TextView boat_on_demand_type_text;
+	private TextView boat_on_demand_categorie_text;
+	private TextView boat_on_demand_chantier_modele_text;
+	private TextView boat_on_demand_etat_text;
+	private TextView boat_on_demand_taille_text;
+	private TextView boat_on_demand_lieu_text;
+	private TextView boat_on_demand_budget_text;
+	private TextView boat_on_demand_commentaire_text;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		
 		View v = inflater.inflate(R.layout.boat_on_demand, container, false);
+		
+		boat_on_demand_type_text = (TextView) v.findViewById(R.id.boat_on_demand_type_text);
+		boat_on_demand_categorie_text = (TextView) v.findViewById(R.id.boat_on_demand_categorie_text);
+		boat_on_demand_chantier_modele_text = (TextView) v.findViewById(R.id.boat_on_demand_chantier_modele_text);
+		boat_on_demand_etat_text = (TextView) v.findViewById(R.id.boat_on_demand_etat_text);
+		boat_on_demand_taille_text = (TextView) v.findViewById(R.id.boat_on_demand_taille_text);
+		boat_on_demand_lieu_text = (TextView) v.findViewById(R.id.boat_on_demand_lieu_text);
+		boat_on_demand_budget_text = (TextView) v.findViewById(R.id.boat_on_demand_budget_text);
+		boat_on_demand_commentaire_text = (TextView) v.findViewById(R.id.boat_on_demand_commentaire_text);
 		
 		return v;
 	}
