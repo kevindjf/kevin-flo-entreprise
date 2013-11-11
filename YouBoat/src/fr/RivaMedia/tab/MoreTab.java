a5c6156d341821c881b954fa948a252f04c4172a
champigny.florent <champigny.florent@gmail.com>
2013-11-11 01:14:27 +0000

    modification des vues, ajout des styles et des couleurs, annonceFormulaire placé dans une activity

--- YouBoat/src/fr/RivaMedia/tab/MoreTab.java
+++ YouBoat/src/fr/RivaMedia/tab/MoreTab.java
@@ -10,16 +10,16 @@ import fr.RivaMedia.R;
 import fr.RivaMedia.tab.core.Tab;
 
 @SuppressLint("ValidFragment")
-public class ActualitesTab extends Tab{
+public class MoreTab extends Tab{
 
-	public ActualitesTab(String titre,Drawable icon) {
+	public MoreTab(String titre,Drawable icon) {
 		super(titre,icon);
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		
-		View v = inflater.inflate(R.layout.splash, container, false);
+		View v = inflater.inflate(R.layout.more, container, false);
 		
 		return v;
 	}
