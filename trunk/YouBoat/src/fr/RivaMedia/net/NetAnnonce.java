3b3baba15b0740b54282757c18088c9f3728f466
champigny.florent@gmail.com <champigny.florent@gmail.com@4e780220-1cfd-1e42-7943-0f62b68f17dc>
2013-11-21 12:47:52 +0000

    ajout des swipe lists

--- trunk/YouBoat/src/fr/RivaMedia/net/NetAnnonce.java
+++ trunk/YouBoat/src/fr/RivaMedia/net/NetAnnonce.java
@@ -11,7 +11,7 @@ import fr.RivaMedia.net.core.Net;
 import fr.RivaMedia.xml.AnnonceXmlParser;
 
 public class NetAnnonce extends Net {	
-	
+
 	public static List<Annonce> rechercher(String url, List<NameValuePair> donnees){
 
 		String xml = Net.requeteGet(url,donnees);
@@ -19,12 +19,34 @@ public class NetAnnonce extends Net {
 		return new AnnonceXmlParser(xml).getListe();
 	}
 
-	public static Annonce rechercherAnnonce(String url, List<NameValuePair> donnees) {
-		List<Annonce> annonces = rechercher(url, donnees);
-		if(annonces.size()>0)
-			return annonces.get(0);
-		else
-			return null;
+	protected static String recupererUrl(String _type){
+		String url = null;
+
+		if(_type != null){
+
+			if(_type.equals(Constantes.BATEAU_A_MOTEUR))
+				url = Constantes.URL_ANNONCES_BATEAUX_A_MOTEURS;
+			else if(_type.equals(Constantes.VOILIER))
+				url = Constantes.URL_ANNONCES_BATEAUX_VOILIERS;
+			else if(_type.equals(Constantes.PNEU))
+				url = Constantes.URL_ANNONCES_BATEAUX_PNEUMATIQUES;
+			else if(_type.equals(Constantes.MOTEURS))
+				url = Constantes.URL_ANNONCES_MOTEURS;
+			else if(_type.equals(Constantes.ACCESSOIRES))
+				url = Constantes.URL_ANNONCES_ACCESSOIRES;
+		}
+
+		return url;
+	}
+
+	public static Annonce rechercherAnnonce(String type, List<NameValuePair> donnees) {
+		String url = recupererUrl(type);
+		if(url != null){
+			List<Annonce> annonces = rechercher(url, donnees);
+			if(annonces.size()>0)
+				return annonces.get(0);
+		}
+		return null;
 	}
 
 	public static List<Annonce> getAnnoncesDe(String numero, String type) {
@@ -35,10 +57,10 @@ public class NetAnnonce extends Net {
 			url = Constantes.URL_ANNONCES_MOTEURS_DE;
 		else if(type.equals(Constantes.ACCESSOIRES))
 			url = Constantes.URL_ANNONCES_ACCESSOIRES_DE;
-		
+
 		return rechercher(url, Net.construireDonnes(
 				Constantes.ANNONCES_ID_CLIENT,numero
 				));
 	}
-	
+
 }
