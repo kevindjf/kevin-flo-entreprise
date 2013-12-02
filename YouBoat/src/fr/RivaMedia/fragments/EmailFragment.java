e7aaf02eb772165b4ff7ef0571f26bd76005e57c
champigny.florent <champigny.florent@gmail.com>
2013-12-02 08:37:12 +0000

    clavier

--- YouBoat/src/fr/RivaMedia/fragments/EmailFragment.java
+++ YouBoat/src/fr/RivaMedia/fragments/EmailFragment.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
